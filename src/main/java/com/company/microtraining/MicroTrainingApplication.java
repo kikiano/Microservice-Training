912d1515454b03c7de317aa2bb2317d730382a5d
Enrique Martinez Salas <kikianohell@hotmail.com>
2021-11-22 16:01:41 -0600

    First commit

---
+++ src/main/java/com/company/microtraining/MicroTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.company.microtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/microtraining/MicroTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.microtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
