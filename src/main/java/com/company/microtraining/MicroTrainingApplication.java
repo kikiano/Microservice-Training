984fbf8e3015013cd2e3f53d31f75ff35adec7ba
Emilio Torres <emilio.trejon@gmail.com>
2021-11-22 17:20:38 -0600

    "Test GitHub Colab. Config"

--- src/main/java/com/company/microtraining/MicroTrainingApplication.java
+++ src/main/java/com/company/microtraining/MicroTrainingApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicroTrainingApplication {
 
+	//Coment
+
 	public static void main(String[] args) {
 		SpringApplication.run(MicroTrainingApplication.class, args);
 	}
